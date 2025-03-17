e3ed6557c3d6dee04219618f9a499cbb0c5e0eaa
Anton1921980 <mywear1@gmail.com>
2025-03-17 21:34:10 +0200

    fix: vercel err

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://order-management-two.vercel.app,
         changeOrigin: true,
       },
     },
